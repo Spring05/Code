1911e16811986135b5d5926c44c23b439ec5dfb8
Spring05 <saranre@hotmail.com>
2019-07-22 20:39:31 -0700

    done

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class MissingNumber {
 
 	public static void main(String[] args) 
 	{
-		int[] array = {1, 2, 3, 4, 5, 7, 8, 9, 10 };
+		//int[] array = {1, 2, 3, 4, 5, 7, 8, 9, 10 };
+		Scanner a =new Scanner(System.in);
+		int[] array =new int[9];
+		System.out.println("Enter numbers");
+		for(int i = 0 ; i <array.length ; i++)	
+		{
+			array[i]=a.nextInt();
+		}
 		int X=array[0];
 		int Y=1;
 		int result;
@@ -15,7 +23,7 @@ public class MissingNumber {
 		
 		for(int i =2 ; i<=array.length+1; i++)
 		{
-			Y	 = Y^ i;
+			Y= Y^ i;
 		}
 		
 		result =  X^Y;
@@ -24,3 +32,16 @@ public class MissingNumber {
 	}
 
 }
+
+//Input Output 
+//Enter numbers
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//Missing number: 10
