616b6bc97c452942beb3053cf221044a8f6efb55
Spring05 <saranre@hotmail.com>
2019-07-22 20:33:33 -0700

    done

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,9 +1,26 @@
 
 public class MissingNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int[] array = {1, 2, 3, 4, 5, 7, 8, 9, 10 };
+		int X=array[0];
+		int Y=1;
+		int result;
+		
+		for(int i =1 ; i<array.length; i++)
+		{
+			X = X^ array[i];
+		}
+		
+		for(int i =2 ; i<=array.length+1; i++)
+		{
+			Y	 = Y^ i;
+		}
+		
+		result =  X^Y;
 
+		System.out.println("Missing number: " + result);
 	}
 
 }
