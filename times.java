f3bb20fe12e3dc6620243c601c65678aa25be050
Spring05 <saranre@hotmail.com>
2019-07-29 20:04:21 -0700

    commit

--- times.java
+++ times.java
@@ -31,7 +31,34 @@ public class times
 		{
 			array[i]=a.nextInt();
 		}
-		 
 		findDuplicates(array,n); 
      }
 }
+
+
+//Test Case 1 
+//Enter the number of elements
+//6
+//Enter numbers
+//1
+//3
+//2
+//3
+//2
+//4
+//Duplicate Element : 3
+//Duplicate Element : 2
+
+
+//test Case 2
+//Enter the number of elements
+//6
+//Enter numbers
+//-1
+//-2
+//-2
+//-6
+//-6
+//-3
+//Duplicate Element : -2
+//Duplicate Element : -6
