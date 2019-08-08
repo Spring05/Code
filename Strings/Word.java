9bcfd76f2a0a5b1c22d0631ed1a64b3ed29e7425
Spring05 <53151140+Spring05@users.noreply.github.com>
2019-08-07 18:57:55 -0700

    Add files via upload

---
+++ Strings/Word.java
@@ -0,0 +1,23 @@
+package New;
+
+import java.util.Scanner;
+
+public class Word 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner a =new Scanner(System.in);
+		System.out.println("Enter the number");
+		int nums;
+		nums= a.nextInt();
+		String s = Integer.toString(nums);
+		System.out.print(s);
+	}
+}
+
+//Test Case
+//Enter the number
+//34
+//34
+
