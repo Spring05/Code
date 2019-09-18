bf573f33224a442e97ce81dd05fff1373eaf686a
Spring05 <53151140+Spring05@users.noreply.github.com>
2019-09-18 16:26:59 -0700

    Add files via upload

---
+++ Array/productArray.java
@@ -0,0 +1,72 @@
+package New;
+
+public class productArray {
+	
+	  void product(int arr[], int n) 
+	    { 
+	  
+	        // Base case 
+	        if (n == 1) { 
+	            System.out.print(0); 
+	            return; 
+	        } 
+	        // Initialize memory to all arrays 
+	        int left[] = new int[n]; 
+	        int right[] = new int[n]; 
+	        int prod[] = new int[n]; 
+	  
+	        int i, j; 
+	  
+	        /* Left most element of left array is always 1 */
+	        left[0] = 1; 
+	  
+	        /* Rightmost most element of right array is always 1 */
+	        right[n - 1] = 1; 
+	  
+	        /* Construct the left array */
+	        for (i = 1; i < n; i++) 
+	            left[i] = arr[i - 1] * left[i - 1]; 
+	  
+	        /* Construct the right array */
+	        for (j = n - 2; j >= 0; j--) 
+	            right[j] = arr[j + 1] * right[j + 1]; 
+	  
+	        /* Construct the product array using  
+	        left[] and right[] */
+	        for (i = 0; i < n; i++)
+	        {
+	            prod[i] = left[i] * right[i]; 
+	        	for(int k = 0 ; k < prod.length;k++)
+	        	{
+	        		if(prod[i]%2==0)
+	        		{
+	        			prod[i]=0;
+	        		}
+	        		
+	        		else
+	        		{
+	        			prod[i]=1;
+	        		}
+	        	}
+		       
+	        }
+	        /* print the constructed prod array */
+	        for (i = 0; i < n; i++) 
+	            System.out.print(prod[i] + " "); 
+	  
+	        return; 
+	    } 
+	  
+	    /* Driver program to test the aboe function */
+	    public static void main(String[] args) 
+	    { 
+	        productArray pa = new productArray(); 
+	        int arr[] = { 3,1,2 }; 
+	        int n = arr.length; 
+	        System.out.println("The product array is : "); 
+	        pa.product(arr, n); 
+	    } 
+	} 
+	  
+
+
